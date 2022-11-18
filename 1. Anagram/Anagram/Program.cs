595109a51fce94c99d23615f4403ffa831f272b3
sosinov <103427734+sosinov@users.noreply.github.com>
2022-11-18 12:36:43 +0200

    upload project

---
+++ 1. Anagram/Anagram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramProject
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string inputSentense = Console.ReadLine();          
+            
+            AnagramClass sentense = new AnagramClass(inputSentense);
+            Console.WriteLine(sentense.Reverse());   
+
+            Console.ReadLine();            
+        }
+    }
+}
